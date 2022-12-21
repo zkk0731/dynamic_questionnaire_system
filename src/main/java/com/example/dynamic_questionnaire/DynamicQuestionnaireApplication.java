40d39227fded7d66fca8336a982ea0cd94f3bb21
hao <you@exampl>
2022-12-21 09:36:07 +0800

    first commit

---
+++ src/main/java/com/example/dynamic_questionnaire/DynamicQuestionnaireApplication.java
@@ -0,0 +1,13 @@
+package com.example.dynamic_questionnaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicQuestionnaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicQuestionnaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dynamic_questionnaire/DynamicQuestionnaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynamic_questionnaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicQuestionnaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
